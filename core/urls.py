061ae2b5b88418a6e9ed67adf1a649d57416c247
Soumyadeep Dutta <43964727+soumyadeepdutta@users.noreply.github.com>
2020-11-17 13:52:51 +0530

    Static changed

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import home, webhook
+from django.conf.urls.static import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('webhook/', webhook, name='webhook'),
     path('', home, name='home'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=STATIC_ROOT)
