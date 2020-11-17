64489cb420867ac49c088a7630586eb18c8b2076
Soumyadeep Dutta <43964727+soumyadeepdutta@users.noreply.github.com>
2020-11-17 15:23:02 +0530

    static filex fixed 1

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
 from .views import home, webhook
-from django.conf.urls.static import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('webhook/', webhook, name='webhook'),
     path('', home, name='home'),
 ]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
