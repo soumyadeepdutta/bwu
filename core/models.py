093423fd90de190490e33d861908d7c3ba7d3ce6
Soumyadeep Dutta <43964727+soumyadeepdutta@users.noreply.github.com>
2020-11-22 08:11:00 +0530

    User Model Created

--- core/models.py
+++ core/models.py
@@ -13,21 +13,32 @@ class DummyProperty(models.Model):
         return self.country + ' ' + str(self.price)
 
 
-class Property(models.Model):
-    country = models.CharField(max_length=10)
-    area = models.TextField(max_length=255, help_text='Area')
-    location = models.CharField(max_length=100)
-    specification = models.CharField(max_length=100, blank=True, null=True)
-    built_year = models.CharField(max_length=30, blank=True, null=True)
-    sold = models.BooleanField(default=False)
+class Response(models.Model):
+    age = models.CharField(max_length=20,  verbose_name='Property Age')
+    bhk = models.IntegerField()
+    typ = models.CharField(max_length=20, blank=True, null=True)
+    price = models.IntegerField()
+
+    class Meta:
+        verbose_name = 'Response Form'
+        verbose_name_plural = 'Response Forms'
+
 
     def __str__(self):
         return self.country + ' ' + self.area
 
 
 class User(models.Model):
-    name = models.CharField(max_length=120)
-    email = models.EmailField()
+    name = models.CharField(max_length=120, verbose_name='User name')
+    email = models.EmailField(verbose_name='Email')
+    contact_no = models.IntegerField(verbose_name='Contact Number')
+    address = models.TextField(verbose_name='Address', max_length=255, blank=True, null=True)
+    city = models.CharField(verbose_name='City', max_length=100, blank=True, null=True)
+    pin_code = models.IntegerField()
+    state=models.CharField(max_length=80, blank=True, null=True)
+    country = models.CharField(max_length=80, blank=True, null=True)
+    date_of_birth = models.DateTimeField(blank=True, null=True)
+    is_agent = models.BooleanField(default=False)
     
 
     def __str__(self):
--- core/save_user.py
+++ core/save_user.py
@@ -3,6 +3,12 @@ from .models import User
 def createUser(params):
     user = User(
         name=params['name'][0],
-        email=params['mail'][0]
+        email=params['mail'][0],
+        contact_no=params['contact'][0],
+        city=params['city'][0],
+        pin_code=params['pin_code'][0],
+        state=params['state'][0],
+        country=params['country'][0],
+        date_of_birth=params['dob'][0],
     )
     user.save()
